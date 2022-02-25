96e8eb43625c1ae267cf181a9deb09c83007adf5
9ony <darsta7@gmail.com>
2022-02-25 18:43:35 +0900

    refactor : Add_Two_Number

--- Add_Two_Number.py
+++ Add_Two_Number.py
@@ -4,31 +4,28 @@ class ListNode:
         self.next = next
 class Solution:
     def addTwoNumbers(self, l1: ListNode, l2: ListNode) -> ListNode:
-        head = ListNode()
-        result = head
-        carry = 0
-        while (l1 or l2 or carry):
-            if l1 and l2 :
-                carry = (result.val + l1.val + l2.val) // 10
-                sum = result.val + l1.val + l2.val - (carry*10)
-                result.val = sum
-            elif l1 or l2 :
-                c = l1 if l1 else l2
-                carry = (result.val + c.val) // 10
-                sum = result.val + c.val - (carry*10)
-            elif carry :
-                sum = carry
-                carry = 0
-                
-            l1 = l1.next if l1 else None
-            l2 = l2.next if l2 else None
-            result.val = sum
-            if l1 or l2 or carry :
-                result.next = ListNode(carry)
-            else : result.next = None
-            result = result.next
-
-        return head
-
-l1 = [1,2,3]
-l2 = [8,7,6]
+        result = ListNode()
+        curr = result
+        carry = 0 #올림 자리 보관
+        
+        while l1 or l2 or carry:
+            curr.next = ListNode()
+            curr = curr.next
+            num1 = num2 = 0
+            if l1 :
+                num1 = l1.val
+                l1 = l1.next
+            if l2 :
+                num2 = l2.val
+                l2 = l2.next
+            curr.val = (num1 + num2 + carry) % 10
+            carry = (num1 + num2 + carry) // 10
+        
+        return result.next
+l1=ListNode(2)
+l1.next=ListNode(4)
+l1.next.next=ListNode(3)
+l2=ListNode(5)
+l2.next=ListNode(6)
+l2.next.next=ListNode(4)
+print(Solution().addTwoNumbers(l1,l2).val)
